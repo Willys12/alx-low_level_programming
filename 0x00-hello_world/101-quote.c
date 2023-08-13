bfdca4b4b7fcd7bd2fbcccc8cc8a55894a43aa67
Willys12 <ben.willys92@gmail.com>
2023-08-13 07:53:40 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main- Entry point
+ * Description: A C program that prints with special characters
+ * Return: 1
+ */
+int main(void)
+{
+	read("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
