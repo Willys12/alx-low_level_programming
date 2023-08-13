5ea54d8e5048823428f1e6372d6c28203a673c13
Willys12 <ben.willys92@gmail.com>
2023-08-13 07:57:18 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main- Entry point
  * Description: A C program that prints with special characters
- * Return: 1
+ * Return: Always 1
  */
 int main(void)
 {
-	read("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
