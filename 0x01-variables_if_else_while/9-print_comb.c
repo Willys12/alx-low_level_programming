df01666ca74ae31819d38ec04dd6f9d5cd7cb406
Willys12 <ben.willys92@gmail.com>
2023-08-14 12:34:43 -0700

    No spacing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; ++i)
+	for (i = 0 ; i < 10 ; ++i)
 	{	putchar(i + '0');
-		putchar(',');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
 	putchar('\n');
 	return (0);
 }
