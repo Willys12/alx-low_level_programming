0e37bcaf417a80df2f457263c2bccc6f68f4f420
Willys12 <ben.willys92@gmail.com>
2023-08-16 15:36:57 -0700

    multiple numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that computes and prints multiples
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int j;
+
+	for (j = 0 ; j < 1024 ; ++j)
+	{
+		if (j % 3 == 0 || j % 5 == 0)
+			sum = sum + j;
+	}
+	printf("%d\n", sum);
+}
