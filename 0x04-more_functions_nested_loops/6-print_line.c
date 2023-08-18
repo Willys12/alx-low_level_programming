89cac6af836c7cfd155b414d0b1bb4d8669e1dfd
Willys12 <ben.willys92@gmail.com>
2023-08-18 13:19:46 -0700

    Line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,13 @@ void print_line(int n)
 {
 	int i;
 
-	n = 100;
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0 ; i < n ; ++i)
+		for (i = 0 ; i <= n ; ++i)
 		{
 			_putchar('_');
 		}
