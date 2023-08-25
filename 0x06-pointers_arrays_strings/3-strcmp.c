55a4c862673f315b44757e38ccffa2e649da584c
Willys12 <ben.willys92@gmail.com>
2023-08-24 23:32:20 -0700

    Correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strcmp - Function begins here
+ * _strcmp - Function begins here
  * @s1: First string
  * @s2: Second string
  * Return: 0;
@@ -8,18 +8,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int k;
-	int l;
 
 	k = 0;
-	l = s1[k] - s2[k];
 	for (k = 0 ; s1[k] != '\0' || s2[k] != '\0' ; ++k)
 	{
 		if (s1[k] != s2[k])
 		{
 			if (s1[k] < s2[k])
-				return (l);
+				return (s1[k] - s2[k]);
 			else if (s2[k] < s1[k])
-				return (l);
+				return (s1[k] - s2[k]);
 		}
 	}
 	return (0);
