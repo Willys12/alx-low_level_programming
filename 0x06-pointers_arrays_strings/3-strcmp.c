aa2d5524db0b0310803e250460332122e522de27
Willys12 <ben.willys92@gmail.com>
2023-08-24 23:43:23 -0700

    Comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[k] != s2[k])
 		{
-			if (s1[k] < s2[k])
-				return (s1[k] - s2[k]);
-			else if (s2[k] < s1[k])
-				return (s1[k] - s2[k]);
+			return (s1[k] - s2[k]);
 		}
 	}
 	return (0);
