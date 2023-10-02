59f1ef8cfed62c6c43a166d5b558cde65713a131
Willys12 <ben.willys92@gmail.com>
2023-10-02 09:21:19 -0700

    Adding two positive numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - This function multiplies two numbers
+*@argc: Argument count
+*@argv: Argument vector
+*Return: Integer
+*/
+int main(int argc, char* argv[])
+{
+	int i, sum = 0, num;	
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
