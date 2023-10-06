0d810cec324d2a8d46f8a19930f0b45be6b82bc7
Willys12 <ben.willys92@gmail.com>
2023-10-06 07:12:14 -0700

    Concatenating two string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+*string_nconcat - concatenates two strings.
+*@s1: String 1.
+*@s2: String 2.
+*@n: Number of bytes
+*Return: Pointer to the concatenated string.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *concat_str;
+unsigned int len_1 = 0;
+unsigned int len_2 = 0;
+unsigned int entire_len = 0;
+unsigned int i;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+len_1 = strlen(s1);
+len_2 = strlen(s2);
+concat_str = malloc(n * sizeof(char));
+if (concat_str == NULL)
+{
+return (NULL);
+}
+if (n >= len_2)
+{
+entire_len = len_1 + len_2;
+}
+else
+{
+entire_len = len_1 + n;
+}
+for (i = 0; i < len_1; i++)
+{
+concat_str[i] = s1[i];
+}
+for (i = 0; i < len_2 && i < n; i++)
+{
+concat_str[len_1 + 1] = s2[i];
+}
+concat_str[entire_len] = '\0';
+
+return (concat_str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 #include <math.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
