007e1e59e5fab5ea8e0af93b5bec8b23fb71b0d7
Willys12 <ben.willys92@gmail.com>
2023-10-10 12:29:56 -0700

    New type structure of dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,9 @@
+# C - Structures, typedef
+
+## Learning Objectives
+
+The objective is to learn about the application of structures & typedef in C.
+
+## General
+- What are structures, when, why and how to use them.
+- How to use typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+*struct dog - It describes the dog.
+*@name: Rep dog's name.
+*@age: Describes dog's age.
+*@owner: Rep dog owner's name.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
