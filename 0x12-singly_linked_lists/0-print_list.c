b2caf626ceec475bbad2bf032f1a3d40f2621e77
Willys12 <ben.willys92@gmail.com>
2023-10-19 14:34:45 -0700

    Prints all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list.
+* @h: Pointer to the node.
+* Return: Number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+size_t nodes = 0;
+
+	for (; h; h = (*h).next, nodes++)
+	{
+		if ((*h).str)
+		{
+			printf("[%u] %s\n", (*h).len, (*h).str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+	}
+return (nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+# C - Singly linked lists
+
+## Learning objective
+- The objective of this project is to learn or get an understanding of singly linked lists.
+
+## General
+- When and why using linked lists vs arrays.
+- How to build and use linked lists.
+
