7f61f64c6e32b769069fff7ffc5462ad89406f42
Willys12 <ben.willys92@gmail.com>
2023-10-20 14:23:50 -0700

    Printing info given

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* before_main - A function that is executed before the main function.
+* Return: void
+*/
+void __attribute__((constructor)) before_main()
+{
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}
+
