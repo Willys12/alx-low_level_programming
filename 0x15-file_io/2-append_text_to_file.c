06cebe2f2b7aef75f0290530d44c4b5a33ed6a54
Willys12 <ben.willys92@gmail.com>
2023-11-01 15:17:12 -0700

    Correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,24 +3,25 @@
 * append_text_to_file -  appends text at the end of a file.
 * @filename: Represents file name.
 * @text_content: Represents what is added.
+* Return: 1 on success and -1 on failure.
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
- FILE *file;
+FILE *file;
 
 if (filename == NULL || text_content == NULL)
 {
-return -1;
+return (-1);
 }
 
 file = fopen(filename, "a");
 if (file == NULL)
 {
-return -1;
+return (-1);
 }
 
 fputs(text_content, file);
 fclose(file);
 
-return 1;
+return (1);
 }
