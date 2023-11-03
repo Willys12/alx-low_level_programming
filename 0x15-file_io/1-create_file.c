2bb937fe2f30acaa90c6728bbd87ff30dc367486
Willys12 <ben.willys92@gmail.com>
2023-11-03 03:14:44 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,6 @@
 int create_file(const char *filename, char *text_content)
 {
 int j;
-mode_t old_msk;
 ssize_t count;
 ssize_t length = 0;
 
@@ -16,11 +15,14 @@ if (!filename)
 {
 return (-1);
 }
-old_msk = umask(0);
-j = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-umask(old_msk);
+j = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 if (j == -1)
 {
+return (-1);
+}
+if (fchmod(j, S_IRUSR | S_IWUSR))
+{
+close(j);
 return (-1);
 }
 if (text_content)
