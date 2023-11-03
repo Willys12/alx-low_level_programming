ada43cd8bcc7edc2cc6b47ecafde85d251a3da99
Willys12 <ben.willys92@gmail.com>
2023-11-03 12:22:55 -0700

    Added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,28 +8,62 @@ int main(int argc, char *argv[]);
 */
 void copy_file(const char *f_first, const char *f_last)
 {
-FILE *source_fl, *dest_fl;
-int f;
+int source_fd, dest_fd;
+ssize_t bytes_read, bytes_written;
+char buffer[BUFFER_SIZE];
 
-source_fl = fopen(f_first, "r");
-if (source_fl == NULL)
+source_fd = open(f_first, O_RDONLY);
+if (source_fd == -1)
 {
-fprintf(stderr, "Error: Can't read from file %s\n", f_first);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_first);
 exit(98);
 }
-dest_fl = fopen(f_last, "w");
-if (dest_fl == NULL)
+
+dest_fd = open(f_last, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+if (dest_fd == -1)
 {
-fclose(source_fl);
-fprintf(stderr, "Error: Can't open file %s\n", f_last);
+close(source_fd);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", f_last);
 exit(99);
 }
-while ((f = fgetc(source_fl)) != EOF)
+
+while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
 {
-fputc(f, dest_fl);
+bytes_written = write(dest_fd, buffer, bytes_read);
+if (bytes_written == -1)
+{
+close(source_fd);
+close(dest_fd);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", f_last);
+exit(99);
 }
-fclose(source_fl);
-fclose(dest_fl);
+}
+
+if (bytes_read == -1)
+{
+close(source_fd);
+close(dest_fd);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_first);
+exit(98);
+}
+
+if (close(source_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
+exit(100);
+}
+
+if (close(dest_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+exit(100);
+}
+
+if (chmod(f_last, 0664) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't set file permissions for %s\n", f_last);
+exit(101);
+}
 
 printf("File %s copied to %s\n", f_first, f_last);
 }
@@ -38,15 +72,16 @@ printf("File %s copied to %s\n", f_first, f_last);
 * main - Entry point.
 * @argc: Argument count.
 * @argv: Argument vector.
-* Return: on success 0.
+* Return: 0 on success.
 */
 int main(int argc, char *argv[])
 {
 const char *f_first;
 const char *f_last;
+
 if (argc != 3)
 {
-fprintf(stderr, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
