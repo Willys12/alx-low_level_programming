724b24eaa2eeb1c4540477f30529607ff6b321ce
Willys12 <ben.willys92@gmail.com>
2023-12-07 13:26:58 -0800

    Given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,54 @@
 #include "lists.h"
 /**
-*
+* insert_dnodeint_at_index - Creates a node at given po.
+* @n: The data.
+* @idx: The position where node is created.
+* @h: Head of list.
+* Return: New node.
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *temp = *h;
-dlistint_t *new_node;
-unsigned int count = 0;
+unsigned int i;
+dlistint_t *newNode;
+dlistint_t *current;
 
-if (idx <= 0)
+   /*Allocate memory for the new node*/
+newNode = malloc(sizeof(dlistint_t));
+if (newNode == NULL)
+return NULL;
+
+/* Assign the data to the new node*/
+newNode->n = n;
+newNode->prev = NULL;
+newNode->next = NULL;
+
+   /* If the list is empty or the index is 0, insert at the beginning*/
+if (*h == NULL || idx == 0)
 {
-add_dnodeint(h, n);
+newNode->next = *h;
+if (*h != NULL)
+(*h)->prev = newNode;
+*h = newNode;
+return newNode;
 }
-else
+
+   /*Traverse the list until the desired position*/
+current = *h;
+for (i = 0; i < idx; i++)
 {
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
+if (current == NULL)
 {
+free(newNode);
 return NULL;
 }
-new_node->n = n;
-while (count < idx - 1 && temp != NULL)
-{
-temp = temp->next;
-count++;
+current = current->next;
 }
-if (temp == NULL || temp->next == NULL)
-{
-new_node->next = NULL;
-new_node->prev = temp;
-if (temp != NULL)
-{
-temp->next = new_node;
-}
-}
-else
-{
-new_node->next = temp->next;
-new_node->prev = temp;
-temp->next->prev = new_node;
-temp->next = new_node;
-}
-}
-return (new_node);
+
+   /*Insert the new node at the desired position*/
+newNode->next = current;
+newNode->prev = current->prev;
+current->prev->next = newNode;
+current->prev = newNode;
+
+return newNode;
 }
